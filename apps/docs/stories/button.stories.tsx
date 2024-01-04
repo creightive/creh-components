bdd0ff35655653bd9c2ac659fae39c067116c60f
George Bockari <bitwhys@gmail.com>
2024-01-04 01:55:13 -0500

    refactor: use CSF v3 format in stories

--- apps/docs/stories/button.stories.tsx
+++ apps/docs/stories/button.stories.tsx
@@ -1,40 +1,22 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from "@acme/ui";
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button } from '@acme/ui'
 
-const meta: Meta<typeof Button> = {
+const meta = {
   component: Button,
-  argTypes: {
-    type: {
-      control: { type: "radio" },
-      options: ["button", "submit", "reset"],
-    },
-  },
-};
+} satisfies Meta<typeof Button>
 
-export default meta;
+export default meta
 
-type Story = StoryObj<typeof Button>;
+type Story = StoryObj<typeof Button>
 
 /*
  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
  * See https://storybook.js.org/docs/react/api/csf
  * to learn how to use render functions.
  */
+
 export const Primary: Story = {
-  render: (props) => (
-    <Button
-      {...props}
-      onClick={(): void => {
-        // eslint-disable-next-line no-alert -- alert for demo
-        alert("Hello from Turborepo!");
-      }}
-    >
-      Hello
-    </Button>
-  ),
-  name: "Button",
   args: {
-    children: "Hello",
-    type: "button",
+    children: 'Hello',
   },
-};
+}
